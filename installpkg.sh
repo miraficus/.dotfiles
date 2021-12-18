271e710bd0736bfeebd016a277202f0c854a508f
Miraficus <63237821+miraficus@users.noreply.github.com>
2021-12-18 18:02:47 +0100

    Update installpkg.sh

--- installpkg.sh
+++ installpkg.sh
@@ -31,7 +31,7 @@ do
         if [[ ${osInfo[$f]} = pacman ]]; then
             echo -e "${GREEN}Installing packages!${NC}\n"
             sudo pacman --needed --ask 4 -Sy - < pkglist.txt
-            paru -S arcolinux-wallpapers-git ttf-ms-fonts sublime-text-4 vscodium otf-raleway ttf-mononoki
+            paru -S arcolinux-wallpapers-git ttf-ms-fonts sublime-text-4 vscodium otf-raleway ttf-mononoki openrgb
 
         else
             echo -e "${RED}Package manager not supported!${NC}"
