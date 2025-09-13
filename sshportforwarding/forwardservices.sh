c3d66b14210f7ae0b09b2506fbfed31445f00613
Miraficus <63237821+miraficus@users.noreply.github.com>
2025-09-13 13:30:06 +0200

    Update forwardservices.sh
    Added landingpage

--- sshportforwarding/forwardservices.sh
+++ sshportforwarding/forwardservices.sh
@@ -11,6 +11,7 @@ do
     -R 0.0.0.0:8123:localhost:8123 \
     -R 0.0.0.0:25575:localhost:25575 \
     -R 0.0.0.0:8124:localhost:8124 \
+    -R 0.0.0.0:80:localhost:64735 \
     -R 0.0.0.0:7777:localhost:7777 \
     -o ExitOnForwardFailure=no \
     -o ConnectTimeout=120 \
